6b5cbb6deeb9fa9392816ae2e4dbc1cab15bad5f
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-06-13 11:45:41 -0500

    fix: adjust Card component styles for better layout and responsiveness

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,14 +4,14 @@ import './Card.css';
 const Card = ({ background, title, icon, text }) => {
   return (
     <article
-      className="card flex flex-col justify-between pt-12 pb-12 px-[49px]"
+      className="card flex flex-col pt-12 pb-12 px-[49px]"
       style={{ backgroundColor: background }}
     >
       <figure className="card__icon">
         <img className="card__icon-svg mb-[35px]" src={icon} alt={`${title} icon`} />
       </figure>
       <h1 className="card__title mb-[25px] text-white uppercase">{title}</h1>
-      <p className="card__paragraph mb-[25px]">{text}</p>
+      <p className="card__paragraph mb-[25px] xl:mb-[83px]">{text}</p>
       <Button background={background} />
     </article>
   );
